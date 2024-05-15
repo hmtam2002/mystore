d2b9e0ce4f70bb7a57c97d4ca35327c662fd89e8
Minh_Tam <113243354+hmtam2002@users.noreply.github.com>
2024-05-15 18:40:03 +0700

    thêm admin

--- admin2/template/news.php
+++
@@ -1,5 +0,0 @@
-<main class="col-md-9 ml-sm-auto col-lg-10 px-md-4 py-4">
-    <?php
-echo "Dây la file news.php"
-?>
-</main>
--- admin2/template/vanphongpham.php
+++
@@ -1,3 +0,0 @@
-<main class="col-md-9 ml-sm-auto col-lg-10 px-md-4 py-4">
-    Từ từ mần cho giống sách
-</main>
